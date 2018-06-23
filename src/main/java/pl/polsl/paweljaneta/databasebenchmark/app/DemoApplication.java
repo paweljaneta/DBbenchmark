bc0bcf960fd9f94637f96abd757aba17c30b7bc5
paweljaneta <paul.007@o2.pl>
2018-06-23 11:58:09 +0200

    postgre connection + component scan

--- src/main/java/pl/polsl/paweljaneta/databasebenchmark/app/DemoApplication.java
+++ src/main/java/pl/polsl/paweljaneta/databasebenchmark/app/DemoApplication.java
@@ -1,12 +1,19 @@
 package pl.polsl.paweljaneta.databasebenchmark.app;
 
+import javafx.geometry.Pos;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import pl.polsl.paweljaneta.databasebenchmark.databaseConnections.PostgreSQLConnection;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "pl.polsl.paweljaneta.databasebenchmark")
 public class DemoApplication {
+    @Autowired
+    static ApplicationContext applicationContext;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
 }
--- src/main/java/pl/polsl/paweljaneta/databasebenchmark/databaseConnections/PostgreSQLConnection.java
+++ src/main/java/pl/polsl/paweljaneta/databasebenchmark/databaseConnections/PostgreSQLConnection.java
@@ -2,6 +2,20 @@ package pl.polsl.paweljaneta.databasebenchmark.databaseConnections;
 
 import org.springframework.stereotype.Component;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @Component
 public class PostgreSQLConnection {
+
+    private Connection connection = null;
+
+    PostgreSQLConnection() {
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:10000/benchmarkDB", "postgres", "admin");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
