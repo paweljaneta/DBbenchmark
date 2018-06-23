0ccd61c44f3d4fef6e8704b1f9680d5f54b62520
paweljaneta <paul.007@o2.pl>
2018-06-23 13:09:59 +0200

    mongodb connection

--- src/main/java/pl/polsl/paweljaneta/databasebenchmark/databaseConnections/MongoDBConnection.java
+++ src/main/java/pl/polsl/paweljaneta/databasebenchmark/databaseConnections/MongoDBConnection.java
@@ -1,7 +1,13 @@
 package pl.polsl.paweljaneta.databasebenchmark.databaseConnections;
 
+import com.mongodb.MongoClient;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MongoDBConnection {
+    MongoClient mongoClient;
+
+    public MongoDBConnection() {
+        this.mongoClient = new MongoClient( "localhost" , 27017 );
+    }
 }
--- src/main/java/pl/polsl/paweljaneta/databasebenchmark/databaseConnections/OracleConnection.java
+++ src/main/java/pl/polsl/paweljaneta/databasebenchmark/databaseConnections/OracleConnection.java
@@ -2,6 +2,19 @@ package pl.polsl.paweljaneta.databasebenchmark.databaseConnections;
 
 import org.springframework.stereotype.Component;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @Component
-public class OracleConnection {
+public class OracleConnection { //TODO: NOT WORKING!!!
+    private Connection connection = null;
+
+    OracleConnection() {
+        try {
+            connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "admin");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
