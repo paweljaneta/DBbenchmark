a43855fe820773e8522bdc4b3d5875e13d2940ed
paweljaneta <paul.007@o2.pl>
2018-09-10 20:57:50 +0200

    remove carts

--- src/main/java/pl/polsl/paweljaneta/databasebenchmark/testScenarios/impl/RemoveObsoleteCartsScenario.java
+++ src/main/java/pl/polsl/paweljaneta/databasebenchmark/testScenarios/impl/RemoveObsoleteCartsScenario.java
@@ -1,10 +1,30 @@
 package pl.polsl.paweljaneta.databasebenchmark.testScenarios.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import pl.polsl.paweljaneta.databasebenchmark.dataConfig.DataConfig;
+import pl.polsl.paweljaneta.databasebenchmark.model.mongo.entities.MongoCart;
+import pl.polsl.paweljaneta.databasebenchmark.model.neo4j.entities.NeoCart;
+import pl.polsl.paweljaneta.databasebenchmark.model.sql.entities.SqlCart;
 import pl.polsl.paweljaneta.databasebenchmark.testScenarios.BaseScenario;
+import pl.polsl.paweljaneta.databasebenchmark.testScenarios.impl.timeMeasure.RemoveObsoleteCartsScenarioMethods;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Component
 public class RemoveObsoleteCartsScenario extends BaseScenario {
+    private DataConfig dataConfig;
+    private RemoveObsoleteCartsScenarioMethods removeObsoleteCartsScenarioMethods;
+    private final int NO_OF_CARTS_TO_DELETE = 10;
+
+    @Autowired
+    public RemoveObsoleteCartsScenario(DataConfig dataConfig, RemoveObsoleteCartsScenarioMethods removeObsoleteCartsScenarioMethods) {
+        this.dataConfig = dataConfig;
+        this.removeObsoleteCartsScenarioMethods = removeObsoleteCartsScenarioMethods;
+    }
+
     @Override
     public void before() {
 
@@ -12,11 +32,55 @@ public class RemoveObsoleteCartsScenario extends BaseScenario {
 
     @Override
     public void execute() {
+        List<SqlCart> sqlCarts = removeObsoleteCartsScenarioMethods.getAllSqlCarts();
+        List<MongoCart> mongoCarts = removeObsoleteCartsScenarioMethods.getAllMongoCarts();
+        List<NeoCart> neoCarts = removeObsoleteCartsScenarioMethods.getAllNeoCarts();
 
+        List<Integer> indexesToDelete = indexesToDelete(sqlCarts.size());
+        List<SqlCart> sqlCartsToDelete = getSqlCartsToDelete(indexesToDelete, sqlCarts);
+        List<MongoCart> mongoCartsToDelete = getMongoCartsToDelete(indexesToDelete, mongoCarts);
+        List<NeoCart> neoCartsToDelete = getNeoCartsToDelete(indexesToDelete, neoCarts);
+
+        removeObsoleteCartsScenarioMethods.removeSqlCarts(sqlCartsToDelete);
+        removeObsoleteCartsScenarioMethods.removeMongoCarts(mongoCartsToDelete);
+        removeObsoleteCartsScenarioMethods.removeNeoCarts(neoCartsToDelete);
     }
 
     @Override
     public void after() {
 
+    }
+
+    private List<Integer> indexesToDelete(int size) {
+        List<Integer> result = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < NO_OF_CARTS_TO_DELETE; i++) {
+            result.add(random.nextInt(size));
+        }
+        return result;
+    }
+
+    private List<SqlCart> getSqlCartsToDelete(List<Integer> indexes, List<SqlCart> carts) {
+        List<SqlCart> result = new ArrayList<>();
+        for (Integer index : indexes) {
+            result.add(carts.get(index));
+        }
+        return result;
+    }
+
+    private List<MongoCart> getMongoCartsToDelete(List<Integer> indexes, List<MongoCart> carts) {
+        List<MongoCart> result = new ArrayList<>();
+        for (Integer index : indexes) {
+            result.add(carts.get(index));
+        }
+        return result;
+    }
+
+    private List<NeoCart> getNeoCartsToDelete(List<Integer> indexes, List<NeoCart> carts) {
+        List<NeoCart> result = new ArrayList<>();
+        for (Integer index : indexes) {
+            result.add(carts.get(index));
+        }
+        return result;
     }
 }
--- src/main/java/pl/polsl/paweljaneta/databasebenchmark/testScenarios/impl/timeMeasure/RemoveObsoleteCartsScenarioMethods.java
+++ src/main/java/pl/polsl/paweljaneta/databasebenchmark/testScenarios/impl/timeMeasure/RemoveObsoleteCartsScenarioMethods.java
@@ -1,8 +1,58 @@
 package pl.polsl.paweljaneta.databasebenchmark.testScenarios.impl.timeMeasure;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import pl.polsl.paweljaneta.databasebenchmark.testScenarios.BaseScenario;
+import pl.polsl.paweljaneta.databasebenchmark.annotations.ExecTimeMeasure;
+import pl.polsl.paweljaneta.databasebenchmark.model.mongo.entities.MongoCart;
+import pl.polsl.paweljaneta.databasebenchmark.model.mongo.repository.MongoCartRepository;
+import pl.polsl.paweljaneta.databasebenchmark.model.neo4j.entities.NeoCart;
+import pl.polsl.paweljaneta.databasebenchmark.model.neo4j.repository.NeoCartRepository;
+import pl.polsl.paweljaneta.databasebenchmark.model.sql.entities.SqlCart;
+import pl.polsl.paweljaneta.databasebenchmark.model.sql.repository.SqlCartRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class RemoveObsoleteCartsScenarioMethods{
+    @Autowired
+    private SqlCartRepository sqlCartRepository;
+    @Autowired
+    private MongoCartRepository mongoCartRepository;
+    @Autowired
+    private NeoCartRepository neoCartRepository;
+
+    @ExecTimeMeasure
+    public List<SqlCart> getAllSqlCarts() {
+        return sqlCartRepository.findAll();
+    }
+
+    @ExecTimeMeasure
+    public void removeSqlCarts(List<SqlCart> carts) {
+        sqlCartRepository.deleteAll(carts);
+    }
+
+    @ExecTimeMeasure
+    public List<MongoCart> getAllMongoCarts() {
+        return mongoCartRepository.findAll();
+    }
+
+    @ExecTimeMeasure
+    public void removeMongoCarts(List<MongoCart> carts) {
+        mongoCartRepository.deleteAll(carts);
+    }
+
+    @ExecTimeMeasure
+    public List<NeoCart> getAllNeoCarts() {
+        List<NeoCart> result = new ArrayList<>();
+        for (NeoCart neoCart : neoCartRepository.findAll()) {
+            result.add(neoCart);
+        }
+        return result;
+    }
+
+    @ExecTimeMeasure
+    public void removeNeoCarts(List<NeoCart> carts) {
+        neoCartRepository.deleteAll(carts);
+    }
 }
